d3289d624b33f877ec0d1f6516d41bd8e2fb9d47
codewhizzhard <topeadeolu5@gmail.com>
2025-07-16 21:03:52 +0100

    turn the sidebar to a fixed height

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,13 +4,13 @@ import { Outlet } from 'react-router-dom'
 import Sidebar from './sidebar'
 const Layout = () => {
   return (
-    <div className='flex flex-col min-h-screen'>
+    <div className='flex flex-col h-screen'>
         <Header />
         <main className='flex flex-grow flex-row pt-18 h-full pb-1 '>
-            <div className='w-[300px] bg-gray-800 text-white border-2 border-t-1 border-white'>
+            <div className=' bg-gray-800 text-white border-2 border-t-1 border-white flex-shrink-0 min-w-[300px]'>
                 <Sidebar />
             </div>
-            <div className='flex-grow p-4 border-2 border-white bg-gray-300'>
+            <div className='flex-grow p-4 border-2 border-white bg-gray-300 overflow-y-auto'>
                 <Outlet />
             </div>
             
